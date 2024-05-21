112c19fcbc36b8531723fdd59839269609060632
srikar kudurmalla <srikar2985969c@gmail.com>
2024-05-21 23:48:14 +0530

    blue gradient background color

--- level-6/app/page.tsx
+++ level-6/app/page.tsx
@@ -2,8 +2,7 @@
 
 export default function Home() {
   return (
-   <>
-   Now building great tool-box for authentication
-   </>
+      <main className="flex h-full flex-col items-center justify-centerbg-[radial-gradient(ellipe_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">Heloo blue
+      </main>
   );
 }
