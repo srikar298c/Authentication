672e1dd01909baf7b81b3e7732d8dac361455d6e
srikar kudurmalla <srikar2985969c@gmail.com>
2024-05-22 14:14:02 +0530

    changes in page

--- level-6/app/page.tsx
+++ level-6/app/page.tsx
@@ -1,8 +1,23 @@
+import {Poppins} from "next/font/google"
+import {cn} from "@/lib/utils";
+import {Button} from "@/components/ui/button";
 
-
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"]
+})
 export default function Home() {
-  return (
-      <main className="flex h-full flex-col items-center justify-centerbg-[radial-gradient(ellipe_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">Heloo blue
-      </main>
-  );
+    return (
+        <>
+            <main
+                className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-500 to-purple-800">
+                <div className="space-y-6">
+                    <h1 className={cn("text-6xl font semi-bold text-white drop-shadow-md", font.className)}> Authentication</h1>
+                    <p className="text-white">Level-6 Authentication</p>
+                    <div><Button variant="secondary" size={"lg"}>Sign Up</Button></div>
+                </div>
+            </main>
+
+        </>
+    );
 }
