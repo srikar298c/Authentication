1e092367fbe914b08091fd4bf415917123c5e76b
srikar kudurmalla <srikar2985969c@gmail.com>
2024-05-22 18:44:01 +0530

    new route

--- level-6/app/page.tsx
+++ level-6/app/page.tsx
@@ -1,6 +1,7 @@
 import {Poppins} from "next/font/google"
 import {cn} from "@/lib/utils";
 import {Button} from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
     subsets: ["latin"],
@@ -8,16 +9,26 @@ const font = Poppins({
 })
 export default function Home() {
     return (
-        <>
+        
             <main
                 className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-500 to-purple-800">
+
                 <div className="space-y-6">
-                    <h1 className={cn("text-6xl font semi-bold text-white drop-shadow-md", font.className)}> Authentication</h1>
+                    <h1 className={cn("text-6xl font semi-bold text-white drop-shadow-md", font.className)}>
+                         Authentication
+                    </h1>
+                
                     <p className="text-white">Level-6 Authentication</p>
-                    <div><Button variant="secondary" size={"lg"}>Sign Up</Button></div>
+                    <div>
+                        <LoginButton >
+
+                        <Button variant="ghost" size="lg" >Sign Up</Button>
+                        </LoginButton>
+                    </div>
+
                 </div>
             </main>
 
-        </>
+       
     );
 }
