c7eb1135b510e784a6d9120a6e738a6369f452ec
srikar kudurmalla <srikar2985969c@gmail.com>
2024-05-25 00:03:00 +0530

    Created Register Schema

--- level-6/schemas/index.ts
+++ level-6/schemas/index.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import * as z  from "zod";
 
 export const LoginSchema =z.object({
     email:z.string().email({
@@ -6,5 +6,16 @@ export const LoginSchema =z.object({
     }),
     password: z.string({
         message: "Password is required"
+    })
+})
+export const RegisterSchema =z.object({
+    email:z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(6,{
+        message: "Minimum 6 characters is required"
+    }),
+    name :z.string().min(1,{
+        message:"Name is requried"
     })
 })
