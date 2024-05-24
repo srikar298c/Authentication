908ed329f0d63d7ced3265e8cf5b5d7646312f5d
srikar kudurmalla <srikar2985969c@gmail.com>
2024-05-24 23:58:28 +0530

    login schema update

--- level-6/schemas/index.ts
+++ level-6/schemas/index.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
 export const LoginSchema =z.object({
-    email:z.string().email(),
-    password: z.string()
+    email:z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string({
+        message: "Password is required"
+    })
 })
