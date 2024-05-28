e9565cdddc6dbcac71d9b8eb729ad8c94f93b42c
srikar kudurmalla <srikar2985969c@gmail.com>
2024-05-28 23:58:43 +0530

    callback implementation

--- level-6/app/(protected)/settings/page.tsx
+++ level-6/app/(protected)/settings/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@/auth"
 
-export const SettingsPage = async()=>{
+export const settings = async()=>{
     const session = await auth();
     return(
         <div>
--- level-6/auth.ts
+++ level-6/auth.ts
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import authConfig from "./auth.config"
  
 const prisma = new PrismaClient()
 export const { auth, handlers, signIn, signOut } = NextAuth({
+    callbacks:{
+
+    },
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
     ...authConfig,
-    providers: [GitHub, Google],
+    providers: [],
 })
