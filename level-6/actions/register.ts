334cb41bd81d5d963fcb6708c68486992a2e3404
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-01 09:54:09 +0530

    sendVerificationEmail in register.ts

--- level-6/actions/register.ts
+++ level-6/actions/register.ts
@@ -6,6 +6,7 @@ import * as  z from "zod";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/users";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values:z.infer<typeof RegisterSchema>)=>{
     const validatedFields =
@@ -33,6 +34,10 @@ export const register = async (values:z.infer<typeof RegisterSchema>)=>{
 
     const verificationToken = await generateVerificationToken(email);
 
+    await sendVerificationEmail(
+        verificationToken.email,
+        verificationToken.token,
+    )
 
     return{ success:" Conformation Email sent"}
     
