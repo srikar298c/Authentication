daddee5e95c707f0abd09c45aaf7d151d1eb16db
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-01 09:51:10 +0530

    sendVerificationEmail

--- level-6/lib/mail.ts
+++ level-6/lib/mail.ts
@@ -1,20 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/EmailTemplate';
-import { Resend } from 'resend';
-
+import email from "next-auth/providers/email";
+import {Resend} from "resend"
 const resend = new Resend(process.env.RESEND_API_KEY);
+export const sendVerificationEmail =async(
+  email: string,
+  token: string,
+)=>{
+  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
-    to: ['delivered@resend.dev'],
-    subject: 'Hello world',
-    react: EmailTemplate({ firstName: 'John' }),
-  });
-
-  if (error) {
-    return res.status(400).json(error);
-  }
-
-  res.status(200).json(data);
-};
+  await resend.emails.send({
+    from:"onboarding@resend.dev",
+    to:email,
+    subject:"Confirm your email",
+    html:`<p>Cick <a href="${confirmLink}"></a> to confirm email.</p>`
+  })
+}
