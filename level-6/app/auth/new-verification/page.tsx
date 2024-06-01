9ccf6b70dd872bccd003fea816c94bc457e6ecda
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-01 15:42:07 +0530

    new route for the verify the users

---
+++ level-6/app/auth/new-verification/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function NewVerificationPage({}: Props) {
+  return (
+    <div>NewVerificationPage</div>
+  )
+}
+
+export default NewVerificationPage
