72538612562b6f8e47c71c9a9775d20f73631745
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-07 00:06:20 +0530

    feat : Navbar component

--- level-6/app/(protected)/layout.tsx
+++ level-6/app/(protected)/layout.tsx
@@ -1,9 +1,12 @@
+import Navbar from "./_components/navbar";
+
 interface ProtectedLayoutProps{
     children : React.ReactNode;
 }
 const ProtectedLayout= ({children}:ProtectedLayoutProps)=>{
     return(
         <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-500 to-gray-800 ">
+            <Navbar/>
             {children}
         </div>
     )
