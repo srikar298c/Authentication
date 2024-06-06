d49778f225afed9e829d4ec532eb30a9a560ad8c
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-07 00:02:47 +0530

    refactor: layout.tsx

--- level-6/app/(protected)/layout.tsx
+++ level-6/app/(protected)/layout.tsx
@@ -3,7 +3,7 @@ interface ProtectedLayoutProps{
 }
 const ProtectedLayout= ({children}:ProtectedLayoutProps)=>{
     return(
-        <div className="h-full w-full ">
+        <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-500 to-gray-800 ">
             {children}
         </div>
     )
