685b267848d97f1254a8b99001d68d88e9abb08d
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-06 23:59:21 +0530

    feat: layout added

---
+++ level-6/app/(protected)/layout.tsx
@@ -0,0 +1,12 @@
+interface ProtectedLayoutProps{
+    children : React.ReactNode;
+}
+const ProtectedLayout= ({children}:ProtectedLayoutProps)=>{
+    return(
+        <div className="h-full w-full ">
+            {children}
+        </div>
+    )
+}
+
+export default ProtectedLayout;
