be8e93400adf7a0114b9c8274e785ed6d2343b55
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-08 22:59:00 +0530

    server action

--- level-6/app/(protected)/admin/page.tsx
+++ level-6/app/(protected)/admin/page.tsx
@@ -4,16 +4,20 @@ import { FormSuccess } from "@/components/form-success";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
 
 
 
 const AdminPage =()=>{
+    const onServerActionClick=()=>{
+        
+    }
     const onApiRouteClick =()=>{
         fetch("/api/admin").then((response)=>{
             if(response.ok){
-                console.log("OKAY")
+                toast.success("Allowed API Route")
             }else{
-
+                toast.error("Forbidden Api Route")
             }
         })
     }
