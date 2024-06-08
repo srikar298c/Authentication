b0979e20af3def6395ba53ceffd0f4f72cfca978
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-08 21:26:08 +0530

    clientpage

--- level-6/components/user-info.tsx
+++ level-6/components/user-info.tsx
@@ -1,5 +1,6 @@
 import { ExtendedUser } from "@/next-auth";
 import { Card, CardContent, CardHeader } from "./ui/card";
+import { Badge } from "./ui/badge";
 
 interface UserInfoProps{
     user?:ExtendedUser;
@@ -59,9 +60,9 @@ export const UserInfo=({
             <p className="text-sm font-medium">
                 Two Factor enabled
             </p>
-            <p className="truncate text-xs max-w-[180px] font-monop-1 bg-slate-100 rounded-md">
-                
-            </p>
+          <Badge variant={user?.isTwoFactorEnabled? "success": "destructive"}>
+            {user?.isTwoFactorEnabled ? "ON":"OFF"}
+            </Badge> 
             </div>
             </CardContent>
         </Card>
