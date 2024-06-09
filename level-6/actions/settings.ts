07f1e24fa21b9742d63a1f5525c4e7306fe13093
srikar kudurmalla <srikar2985969c@gmail.com>
2024-06-09 19:13:29 +0530

    server actions for Settings page

---
+++ level-6/actions/settings.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { getUserById } from "@/data/users";
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { SettingsSchema } from "@/schemas";
+import * as z from "zod";
+
+export const settings = async(
+    values: z.infer<typeof  SettingsSchema>
+)=>{
+    const user = await currentUser();
+    
+    if(!user) {
+        return {error:"Unauthorized"}
+    }
+   const dbUser = await getUserById(user.id); 
+
+   if(!dbUser){
+    return {error: "Unauthorized"}
+   }
+   await db.user.update({
+    where:{id: dbUser.id},
+    data:{
+        ...values,
+    }
+    })
+return {success:"Settings Update"}
+}
